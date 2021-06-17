69002e4d2ca04818428b0e8d2d86c763c8d038e6
GorohovDmitriy <dmitriy2893@gmail.com>
2021-06-17 12:21:09 +0300

    Убрал outline input

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -2,20 +2,19 @@ import React from 'react'
 
 const Weather = (props) => {
 	return (
-		<div className="info-list">
-			{
-				props.city &&
-				<div className="info">
-					<p>Местоположение: {props.city}, {props.country}</p>
+		<div className='info-list'>
+			{props.city && (
+				<div className='info'>
+					<p>
+						Местоположение: {props.city}, {props.country}
+					</p>
 					<p>Температура: {props.temp} &#176;C</p>
 					<p>Давление: {props.pressure}</p>
-					<p>Заход солнца: {props.sunset} </p>
 				</div>
-			}
+			)}
 			<p> {props.error} </p>
 		</div>
 	)
 }
 
-export default Weather;
-
+export default Weather
